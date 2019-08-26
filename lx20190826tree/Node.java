701fca90e2d14e63fab070f421afed94828b5a1c
Enight <3063976212@qq.com>
2019-08-26 16:38:09 +0800

    二叉树的综合(递归 非递归)遍历

---
+++ lx20190826tree/Node.java
@@ -0,0 +1,17 @@
+package lx20190826tree;
+
+
+
+	//二叉树的基本结点
+	public  class Node { 
+			Object data;
+			Node leftChild;
+			Node rightChild;
+			public Node(Object data, Node leftChild, Node rightChild) {
+				super();
+				this.data = data;
+				this.leftChild = leftChild;
+				this.rightChild = rightChild;
+			}
+		}
+
